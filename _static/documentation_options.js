3b3ea8a4022d26faf22cb44237ca50e7e1e64071
evamaxfield <evamaxfieldbrown@gmail.com>
2022-08-24 06:30:38 +0000

    Deploying to gh-pages from @ bioio-devs/bioio-types@f522d757be5ad9f854938343eaf02c5fe2c8febf 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g7f54841',
+    VERSION: '0.1.dev1+gf522d75',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
