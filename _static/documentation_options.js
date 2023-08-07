a0718a193aca600c93b71faf5ebf3996bc6c10a3
SeanLeRoy <41307451+SeanLeRoy@users.noreply.github.com>
2023-08-07 18:42:06 +0000

    Deploying to gh-pages from @ bioio-devs/bioio-base@7579d85a226e13aa35af0e395e270910e6568610 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g8b5f1d2',
+    VERSION: '0.1.dev1+g7579d85',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
