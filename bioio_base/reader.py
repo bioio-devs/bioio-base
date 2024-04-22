92b4f16678096bfabf0e159839e255026c54dadb
Dan Toloudis <danielt@alleninstitute.org>
2024-04-22 09:29:10 -0700

    reset on resolution level change

--- bioio_base/reader.py
+++ bioio_base/reader.py
@@ -310,6 +310,9 @@ class Reader(ImageContainer, ABC):
             )
 
         self._current_resolution_level = resolution_level
+        # Reset self for future read
+        # Note that this resets metadata as well.
+        self._reset_self()
 
     @abstractmethod
     def _read_delayed(self) -> xr.DataArray:
