4ce663c591ddbd24631863a5323d3a7ddedcc513
Brian Whitney <brian.whitney@alleninstitute.org>
2025-04-11 09:37:42 -0700

    feature/PPS-Null-reset

--- bioio_base/reader.py
+++ bioio_base/reader.py
@@ -245,7 +245,7 @@ class Reader(ImageContainer, ABC):
         self._mosaic_xarray_data = None
         self._dims = None
         self._metadata = None
-        self._physical_pixel_sizes = PhysicalPixelSizes(None, None, None)
+        self._physical_pixel_sizes = None
 
     def set_scene(self, scene_id: Union[str, int]) -> None:
         """
