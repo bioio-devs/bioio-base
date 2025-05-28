a59b7d98c2d1fe4961f1003f0bd128eedba6d7e9
SeanLeRoy <seanme97@gmail.com>
2025-05-28 16:06:18 -0700

    Add dependencies necessary for grabbing http files

--- bioio_base/tests/test_benchmark.py
+++ bioio_base/tests/test_benchmark.py
@@ -30,13 +30,13 @@ def test_benchmark_local_file() -> None:
         bioio_base.benchmark.cleanup()
 
 
-def test_benchmark_s3_file() -> None:
+def test_benchmark_http_file() -> None:
     try:
         # Arrange
-        s3_file = "https://gettylargeimages.s3.amazonaws.com/00094701.jpg"
+        http_file = "https://gettylargeimages.s3.amazonaws.com/00094701.jpg"
 
         # Act
-        bioio_base.benchmark.benchmark(bioio_base.noop_reader.NoopReader, [s3_file])
+        bioio_base.benchmark.benchmark(bioio_base.noop_reader.NoopReader, [http_file])
 
         # Assert: Run some simple asserts to check output exists
         output_destination = pathlib.Path("output.csv")
